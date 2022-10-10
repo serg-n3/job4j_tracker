2ab597997a99ccdad5bdf5f7ca66060416028ef0
ktg95 <ktg80@yandex.ru>
2022-10-10 21:07:39 +0300

    Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,33 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book book1 = new Book("Clean code", 11);
+        Book book2 = new Book("Dont touch", 12);
+        Book book3 = new Book("My book", 13);
+        Book book4 = new Book("Epic", 14);
+        Book[] books = new Book[4];
+        books[0] = book1;
+        books[1] = book2;
+        books[2] = book3;
+        books[3] = book4;
+        for (int i = 0; i < books.length; i++) {
+            Book bk = books[i];
+            System.out.println(bk.getName() + "-" + bk.getCount());
+        }
+        Book bk1 = new Book(" ", 0);
+        bk1 = books[0];
+        books[0] = books[3];
+        books[3] = bk1;
+        for (int i = 0; i < books.length; i++) {
+            Book bk = books[i];
+            System.out.println(bk.getName() + "-" + bk.getCount());
+        }
+        for (int i = 0; i < books.length; i++) {
+            Book bk = books[i];
+            if (bk.getName().equals("Clean code")) {
+                System.out.println(bk.getName() + "-" + bk.getCount());
+            }
+        }
+    }
+}
