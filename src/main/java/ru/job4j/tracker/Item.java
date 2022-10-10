b240142567886d4e183213bf684b9b43ea478e11
ktg95 <ktg80@yandex.ru>
2022-10-10 15:33:07 +0300

    Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,13 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
+import java.time.format.DateTimeFormatter;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
     }
@@ -20,6 +25,10 @@ public class Item {
         return id;
     }
 
+    public LocalDateTime getTime() {
+        return created;
+    }
+
     public void setId(int id) {
         this.id = id;
     }
@@ -30,5 +39,14 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+}
+
+class StartUI {
+    public static void main(String[] args) {
+        Item obj = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTimeFormat = obj.getTime().format(formatter);
+        System.out.println(currentDateTimeFormat);
     }
 }
