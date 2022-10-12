1c5c0141ee31aff77dc32ce8381411ed06a8317f
ktg95 <ktg80@yandex.ru>
2022-10-12 21:26:41 +0300

    Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -13,16 +13,30 @@ public class Tracker {
         return item;
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
         return rsl;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    public boolean replace(int id, Item item) {
+        boolean rls = indexOf(id) != -1;
+        if (rls) {
+            int index = indexOf(id);
+            item.setId(id);
+            items[index] = item;
+        }
+        return rls;
     }
 
     public Item[] findAll() {
