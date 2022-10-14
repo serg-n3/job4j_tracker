f8f64882094be2a541730724cd2cdf69daa373ad
serg-n3 <ktg80@yandex.ru>
2022-10-14 21:56:14 +0300

    проба

--- src/main/java/ru/job4j/io/MagicBall.java
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -13,7 +13,7 @@ public class MagicBall {
         switch (answer) {
             case 0 -> System.out.println("Да");
             case 1 -> System.out.println("Нет");
-            default -> System.out.println("Может быть");
+            default -> System.out.println("Может быть!");
         }
     }
 }
