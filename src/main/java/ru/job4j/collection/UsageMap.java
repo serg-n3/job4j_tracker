40e69d2f585f56636c8f88aa1894247320bf1967
serg-n3 <ktg80@yandex.ru>
2022-11-18 18:27:24 +0300

    Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("ivanov@yandex.ru", "Ivan Ivanov");
+        map.put("sidorov@yandex.ru", "Petr Sidorov");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
