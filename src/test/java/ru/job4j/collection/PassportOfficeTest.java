8cf941b42530ccc507c1d9eaedf66a0ca2c880ec
serg-n3 <ktg80@yandex.ru>
2022-11-18 20:51:40 +0300

    Паспорт и жители [#212656]

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PassportOfficeTest {
+    @Test
+    public void whenTestAddMethod() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
+    }
+
+    @Test
+    public void whenPassportAlwaysInSystem() {
+        Citizen citizen1 = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen1);
+        Citizen citizen2 = new Citizen("2f44a", "Petr Ivanov");
+        assertThat(office.add(citizen2)).isFalse();
+    }
+}
