ccd8239a89f310371be241761bdccc4d28a45b06
serg-n3 <ktg80@yandex.ru>
2022-12-01 15:44:32 +0300

    Сортировка [#10096]

--- src/main/java/ru/job4j/tracker/ItemDescByName.java
+++ src/main/java/ru/job4j/tracker/ItemDescByName.java
@@ -2,7 +2,7 @@ package ru.job4j.tracker;
 
 import java.util.Comparator;
 
-public class ItemDescByName implements  Comparator<Item> {
+public class ItemDescByName implements Comparator<Item> {
     @Override
     public int compare(Item left, Item right) {
         return right.getName().compareTo(left.getName());
